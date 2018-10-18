da39a2eef002721547376239a06e2e1d103ee0b5
Suvajit Chakrabarty <abhidec1990@gmail.com>
2018-10-18 19:44:28 -0400

    Commit master

--- src/components/FoodTruckDashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const FoodTruckDashboard = () => (
-  <div className="content-container">
-    <p>This will be our foodtruck dashboard!</p>
-  </div>
-);
-
-export default FoodTruckDashboard;
